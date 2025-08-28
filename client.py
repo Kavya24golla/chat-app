411a4740db8c74d84ebefb5046758f5befceec64
Kavya24golla <kavyagolla1234@gmail.com>
2025-08-29 01:21:49 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,21 +6,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-# Ensure chat log exists
+
 if not os.path.exists("chat_log.txt"):
     open("chat_log.txt", "w").close()
 
-# Serve chat page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Handle incoming messages
 @socketio.on('message')
 def handle_message(msg):
     print(msg)
     send(msg, broadcast=True)
-    # Save chat to log
+    
     with open("chat_log.txt", "a", encoding="utf-8") as f:
         f.write(msg + "\n")
 
